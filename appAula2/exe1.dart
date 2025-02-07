72379d2722c765809c71600dfd200433e83996b7
Murilosxv <murilo.sxv@gmail.com>
2025-02-06 21:54:46 -0300

    aula 2 dart
    Fundamentos Básicos

---
+++ appAula2/exe1.dart
@@ -0,0 +1,18 @@
+// import a biblioteca dart io 
+import 'dart:io';
+//
+// função principal do codigo 
+void main(){
+  // variavel do tipo inteiro 
+  int idade = 19;
+  print("Idade: $idade");
+  //Variavel double 
+  double raio = 10.25;
+  print("raio $raio");
+  // Variavel do tipo String
+  String nome = "Murilo";
+  print("Ola $nome ! Seja bem-vindo");
+  //Variavel boleana 
+  bool ligado = true;
+  print("Status $ligado");
+}
---
+++ appAula2/exe2.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+  //escreve mensagem no terminal 
+  print("Digite seu nome:");
+  String nome = stdin.readLineSync()!;
+  print("$nome");
+  //escreve mensagem no terminal 
+  print("Digite sua idade");
+  String idadestring = stdin.readLineSync()!;
+  print("Idade: $idadestring");
+}
