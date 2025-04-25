1d802a6800a3f73b98b56da8d892514e5353f042
Murilosxv <murilo.sxv@gmail.com>
2025-04-24 22:16:02 -0300

    primeiro commit

---
+++ appAula12/aula24_04/android/app/src/main/kotlin/com/exampleappaula8/aula24_04/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleappaula8.aula24_04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ appAula12/aula24_04/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:aula24_04/home.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+void main() {
+  runApp(TelaApp());
+}
+
+class TelaApp extends StatelessWidget {
+  const TelaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme:ColorScheme.fromSeed(seedColor: Colors.teal),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor:Colors.red,
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12)
+            )
+          )
+        )
+      ),
+      home: AnimatedSplashScreen(
+        duration: 3000, // duração em segundos
+        splash: Icons.home, // Poderia ser imagem, texto
+        splashIconSize: 200, // aumenta o tamanho do icone
+        splashTransition: SplashTransition.scaleTransition,
+         pageTransitionType: PageTransitionType.leftToRight,
+         nextScreen: Home(), // proxima tela
+         backgroundColor: Colors.red,
+         
+         
+         )
+      
+       
+    );
+  }
+}
